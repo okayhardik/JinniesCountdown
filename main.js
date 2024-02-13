8fd6b276c1974a7de4b58a398efb38f900822379
okayhardik <154338672+okayhardik@users.noreply.github.com>
2024-02-13 20:32:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Set the birthday date (replace with the actual birthday date)
+    const birthdayDate = new Date('2024-03-10T00:00:00');
 
+    function updateCountdown() {
+        const currentDate = new Date();
+        const timeDifference = birthdayDate - currentDate;
+
+        if (timeDifference > 0) {
+            const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+            document.getElementById('days').textContent = formatTime(days);
+            document.getElementById('hours').textContent = formatTime(hours);
+            document.getElementById('minutes').textContent = formatTime(minutes);
+            document.getElementById('seconds').textContent = formatTime(seconds);
+        } else {
+            // Birthday has passed, update the message
+            document.getElementById('contentContainer').innerHTML = "<p>Happy Birthday, Jinnie!</p>";
+            document.getElementById('countdown').style.display = 'none';
+        }
+    }
+
+    function formatTime(time) {
+        return time < 10 ? `0${time}` : time;
+    }
+
+    function changeBackground() {
+        const section = document.getElementById('birthdaySection');
+        section.style.backgroundImage = `url('${getRandomImage()}')`;
+    }
+
+    function getRandomImage() {
+        const images = [
+            'Countdown\WhatsApp Image 2024-02-13 at 7.58.54 PM (1).jpeg',
+            'Countdown\WhatsApp Image 2024-02-13 at 7.58.54 PM.jpeg',
+            // Add more image filenames as needed
+        ];
+
+        const randomIndex = Math.floor(Math.random() * images.length);
+        return images[randomIndex];
+    }
+
+    // Initial call to update countdown and background on page load
+    updateCountdown();
+    changeBackground();
+
+    // Call updateCountdown every second
+    setInterval(updateCountdown, 1000);
+    // Call changeBackground every 5 minutes (adjust the interval as needed)
+    setInterval(changeBackground, 30000);
+});
